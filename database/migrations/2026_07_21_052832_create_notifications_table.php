c48b02f624af2b48bf74ce17069584e8f452206b
camp_management <camp_management@local>
2026-07-22 09:15:51 +0300

    fix errors

--- database/migrations/2026_07_21_052832_create_notifications_table.php
+++ database/migrations/2026_07_21_052832_create_notifications_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('notifications', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('type');
             $table->string('notifiable_type');
             $table->unsignedBigInteger('notifiable_id');
